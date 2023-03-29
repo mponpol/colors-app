96edd6a63eb6a9c8265df2b8049e45bb577810d5
mponpol <mponpol@gmail.com>
2023-03-29 12:42:33 +0200

    Add Palette

---
+++ src/Palette.jsx
@@ -0,0 +1,11 @@
+function Palette() {
+  return (
+    <div className="Palette">
+      {/* Navbar goes here */}
+      <div className="Palette-colors">{/* bunch of color boxes */}</div>
+      {/* footer eventually */}
+    </div>
+  );
+}
+
+export default Palette;

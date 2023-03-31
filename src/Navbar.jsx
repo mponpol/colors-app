cedc188622e7d325d07a082c811ce24d8c60bb5d
Mario Ponce <mponpol@gmail.com>
2023-04-01 00:20:00 +0200

    Add Snackbar and Style Select

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,10 +1,25 @@
+import { useState } from 'react';
 import Select from '@mui/material/Select';
 import { MenuItem } from '@mui/material';
+import Snackbar from '@mui/material/Snackbar';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
 import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
 import './Navbar.css';
 
-function Navbar({ level, setLevel, format, handleChange }) {
+function Navbar({
+  level,
+  setLevel,
+  format,
+  openSnackbar,
+  setOpenSnackbar,
+  handleFormatChange,
+}) {
+  const closeSnackbar = () => {
+    setOpenSnackbar(false);
+  };
+
   return (
     <nav className="Navbar">
       <div className="logo">
@@ -23,12 +38,34 @@ function Navbar({ level, setLevel, format, handleChange }) {
         </div>
       </div>
       <div className="select-container">
-        <Select value={format} onChange={handleChange}>
+        <Select value={format} onChange={handleFormatChange}>
           <MenuItem value="hex">HEX - #ffffff</MenuItem>
           <MenuItem value="rgb">RGB - rgb(255,255,255)</MenuItem>
           <MenuItem value="rgba">RGBA - rgb(255,255,255,1.0)</MenuItem>
         </Select>
       </div>
+      <Snackbar
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
+        open={openSnackbar}
+        autoHideDuration={3000}
+        message={
+          <span id="message-id">Format Changed to {format.toUpperCase()}</span>
+        }
+        ContentProps={{
+          'aria-describedby': 'message-id',
+        }}
+        onClose={closeSnackbar}
+        action={[
+          <IconButton
+            onClick={closeSnackbar}
+            color="inherit"
+            key="close"
+            aria-label="close"
+          >
+            <CloseIcon />
+          </IconButton>,
+        ]}
+      />
     </nav>
   );
 }
--- src/Palette.jsx
+++ src/Palette.jsx
@@ -6,19 +6,23 @@ import './Palette.css';
 function Palette({ palette }) {
   const [level, setLevel] = useState(500);
   const [format, setFormat] = useState('hex');
+  const [openSnackbar, setOpenSnackbar] = useState(false);
   const colorBoxes = palette.colors[level].map(color => (
     <ColorBox background={color[format]} name={color.name} />
   ));
   const changeFormat = evt => {
     setFormat(evt.target.value);
+    setOpenSnackbar(true);
   };
   return (
     <div className="Palette">
       <Navbar
         level={level}
         setLevel={setLevel}
-        handleChange={changeFormat}
+        handleFormatChange={changeFormat}
         format={format}
+        openSnackbar={openSnackbar}
+        setOpenSnackbar={setOpenSnackbar}
       />
       <div className="Palette-colors">{colorBoxes}</div>
       {/* footer eventually */}
